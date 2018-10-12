ba2a42bbec99d9439c4225572cbaa6caa373ead1
fernando.schwambach <fnandos1987@gmail.com>
2018-10-12 11:16:23 -0300

    login

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthenticationService } from '../../services/authentication.service';
 import { AlertController } from '@ionic/angular';
 
@@ -14,18 +14,45 @@ const SERVER_URL = environment.serverUrl;
 export class LoginPage implements OnInit {
 
   registerCredentials = { login: '', pass: '' };
+  alertPrompt;
 
-  constructor(private authService: AuthenticationService, private http: HttpClient, public alertController: AlertController) { }
+  constructor(private authService: AuthenticationService,
+              private http: HttpClient,
+              public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.login('2');
+    this.http.post(`${SERVER_URL}/usuario/logar`, this.registerCredentials)
+      .toPromise()
+      .then(response => {
+        if (response) {
+          this.authService.login(response);
+        } else {
+          this.presentAlert('Login não realizado', 'Não foi possível realizar a autenticação. Digite novamente seu usuário e senha.');
+        }        
+      })
+      .catch(err =>{
+        this.presentAlert('Ops!', 'Aparentemente estamos com probemas na conexão com o servidor. Tente novamente mais tarde');
+      });
+  }
+
+  verificarEmail(email) {
+    this.http.post(`${SERVER_URL}/usuario/novasenha`, {email: email})
+      .toPromise()
+      .then(response => {       
+        if (response) {
+          this.alertPrompt.dismiss();
+          this.presentAlert('Nova senha enviada', 'Sua nova senha foi enviada para seu email com sucesso.');
+        } else {
+          this.presentAlert('Usuário não encontrado', 'Esta email não foi encontrado na base de dados.');
+        }
+      });
   }
 
   async presentAlertPrompt() {
-    const alert = await this.alertController.create({
+    this.alertPrompt = await this.alertController.create({
       header: 'Esqueceu a senha?',
       message: 'Digite seu email abaixo',
       inputs: [
@@ -44,19 +71,14 @@ export class LoginPage implements OnInit {
         }, {
           text: 'OK',
           handler: data => {
-            if (data.email == 'fernando@unidavi.edu.br') {
-              this.presentAlert('Nova senha enviada', 'Sua nova senha foi enviada para seu email com sucesso.');
-              return true;
-            } else {
-              this.presentAlert('Usuário não encontrado', 'Esta email não foi encontrado na base de dados.');
-              return false;
-            }
+            this.verificarEmail(data.email);
+            return false;
           }
         }
       ]
     });
 
-    await alert.present();
+    await this.alertPrompt.present();
   }
 
   async presentAlert(header, message) {
